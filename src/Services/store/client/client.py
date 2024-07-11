d97bc923d595f5dee96e07a3e42555c15c0db428
IgorJoaquimn <igor-joaquim@ufmg.br>
2024-07-11 16:03:47 -0300

    Folder structure and initial protos

---
+++ src/Services/store/client/client.py
@@ -0,0 +1,10 @@
+from __future__ import print_function # usado internamente nos stubs
+import os # para usar getpid
+
+import grpc
+
+def run():
+    pass
+
+if __name__ == '__main__':
+    run()
---
+++ src/Services/store/server/server.py
@@ -0,0 +1,6 @@
+from concurrent import futures # usado na definição do pool de threads
+import grpc
+def serve():
+   pass
+if __name__ == '__main__':
+    serve()
